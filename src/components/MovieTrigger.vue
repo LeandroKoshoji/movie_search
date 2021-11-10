cbfe76e7c5e53bc1227a272f7ea84561abc8e899
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-11-10 00:02:37 -0300

    feat: create movie trigger component scaffold

---
+++ src/components/MovieTrigger.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="movie-nav">
+    <div class="movie-nav--item">
+      <label for="popular">Populares</label>
+      <input type="radio" name="movie-trigger" id="popular" value="popular" />
+    </div>
+    <div class="movie-nav--item">
+      <label for="topRated">Melhores</label>
+      <input type="radio" name="movie-trigger" id="topRated" value="topRated" />
+    </div>
+    <div class="movie-nav--item">
+      <label for="onTheather">Nos Cinemas</label>
+      <input
+        type="radio"
+        name="movie-trigger"
+        id="onTheather"
+        value="onTheather"
+      />
+    </div>
+    <div class="movie-nav--item">
+      <label for="upComming">Em Breve</label>
+      <input
+        type="radio"
+        name="movie-trigger"
+        id="upComming"
+        value="upComming"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieTrigger",
+};
+</script>
+
+<style lang="scss" scoped></style>
