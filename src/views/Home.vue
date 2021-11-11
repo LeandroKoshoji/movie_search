bbc595cd487884f2516138ad47c4db1a86b9bdd3
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-11-10 21:37:28 -0300

    feat: create movies grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="home">
     <div class="container">
+      <Searchers />
       <MovieTrigger />
+      <div class="movies-grid">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import MovieTrigger from "../components/MovieTrigger.vue";
+import Searchers from "../components/Searchers.vue";
+import MovieCard from "../components/MovieCard.vue";
 export default {
   name: "Home",
-  components: { MovieTrigger },
+  components: { MovieTrigger, Searchers, MovieCard },
 };
 </script>
 
 <style lang="scss" scoped>
 .home {
   min-height: calc(100vh - 100px);
+  padding-top: 3rem;
+
+  .movies-grid {
+    padding-top: 3rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 200px);
+    gap: 1rem;
+    row-gap: 2rem;
+    justify-content: center;
+  }
 }
 </style>
