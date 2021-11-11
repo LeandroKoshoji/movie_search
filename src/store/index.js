693e3dac9e673b714e9c3869fbe9939a50593b19
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-11-11 01:54:25 -0300

    feat: create movies State and fetch funciontions

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,52 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { API_KEY, BASE_URL } from "../utils/api.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    moviesInDisplay: [],
+    loading: false,
+    error: {
+      isError: false,
+      errorMessage: "",
+    },
+    pages: "",
+  },
+  mutations: {
+    setMoviesInDisplay(state, movies) {
+      state.moviesInDisplay = movies;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+    setError(state, payload) {
+      state.error.isError = true;
+      state.error.errorMessage = payload.message;
+    },
+    setPages(state, pages) {
+      state.pages = pages;
+    },
+  },
+  actions: {
+    async fetchTriggeredMovies({ commit }, payload) {
+      try {
+        console.log("FUNCAO:", payload.trigger, payload.page);
+        commit("setLoading", true);
+        const res = await fetch(
+          `${BASE_URL}${payload.trigger}?api_key=${API_KEY}&language=pt-BR&region=br&page=${payload.page}`
+        );
+        const data = await res.json();
+        console.log("trigger:", data);
+        commit("setMoviesInDisplay", data.results);
+        commit("setPages", data.total_pages);
+      } catch (err) {
+        commit("setError", err);
+      } finally {
+        commit("setLoading", false);
+      }
+    },
+  },
   modules: {},
 });
---
+++ src/utils/api.js
@@ -0,0 +1,2 @@
+export const API_KEY = "919cd1b07cfe1a0e19eae82c0d996b72";
+export const BASE_URL = "https://api.themoviedb.org/3/movie/";
