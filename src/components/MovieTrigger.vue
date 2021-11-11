ef42eeeb8278685b70fc68e52f0628dc125a1143
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-11-10 21:38:21 -0300

    Update MovieTrigger.vue

--- src/components/MovieTrigger.vue
+++ src/components/MovieTrigger.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="movie-nav">
     <div class="movie-nav--item">
-      <input type="radio" name="movie-trigger" id="popular" value="popular" />
+      <input
+        type="radio"
+        name="movie-trigger"
+        id="popular"
+        value="popular"
+        v-model="triggerRadio"
+      />
       <label for="popular">Populares</label>
     </div>
     <div class="movie-nav--item">
-      <input type="radio" name="movie-trigger" id="topRated" value="topRated" />
+      <input
+        type="radio"
+        name="movie-trigger"
+        id="topRated"
+        value="topRated"
+        v-model="triggerRadio"
+      />
       <label for="topRated">Melhores</label>
     </div>
     <div class="movie-nav--item">
@@ -14,6 +26,7 @@
         name="movie-trigger"
         id="onTheather"
         value="onTheather"
+        v-model="triggerRadio"
       />
       <label for="onTheather">Nos Cinemas</label>
     </div>
@@ -23,6 +36,7 @@
         name="movie-trigger"
         id="upComming"
         value="upComming"
+        v-model="triggerRadio"
       />
       <label for="upComming">Em Breve</label>
     </div>
@@ -32,25 +46,32 @@
 <script>
 export default {
   name: "MovieTrigger",
+  data() {
+    return {
+      triggerRadio: "",
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .movie-nav {
-  margin: 0 auto;
   text-align: center;
-  width: 100%;
-  padding: 0.5rem, 0;
-  border-radius: 5px;
 
   &--item {
-    display: inline-flex;
+    display: inline-block;
     background-color: var(--clr-accent);
     color: var(--clr-background);
+    border-radius: 5px;
+    margin: 0.5rem 0;
+
+    &:nth-child(-n + 3) {
+      margin-right: 0.2rem;
+    }
 
     label {
       position: relative;
-      padding: 0.25rem 0.5rem;
+      padding: 0 0.5rem;
       transition: background linear 0.5s;
       cursor: pointer;
     }
@@ -67,6 +88,7 @@ export default {
         border-top: 1px solid white;
         border-bottom: 1px solid white;
         border: 2px solid var(--clr-accent);
+        border-radius: 5px;
       }
     }
   }
