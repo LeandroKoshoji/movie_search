386961ff3c204a11710254300b572ec4d9e59fb1
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-11-14 23:18:34 -0300

    feat: create loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loading">
+    <i class="fas fa-spinner"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+};
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  i {
+    color: var(--clr-accent);
+    font-size: 5rem;
+    animation: rotate infinite 1s;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>
