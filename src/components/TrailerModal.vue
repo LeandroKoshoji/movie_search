20d0750c271e21267d29ef16375b06c9e8f2ab92
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-11-16 01:06:49 -0300

    feat: add dinamic iframe to trailer component

--- src/components/TrailerModal.vue
+++ src/components/TrailerModal.vue
@@ -1,36 +1,66 @@
 <template>
-  <div class="trailer-container">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/-BI6OOYqrxE"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+  <div class="wrapper-out" @click="closeModal()">
+    <div class="wrapper-inner">
+      <div class="trailer-container">
+        <iframe
+          width="560"
+          height="315"
+          :src="`https://www.youtube.com/embed/${link}`"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TrailerModal",
+  props: {
+    link: {
+      required: true,
+      type: String,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.trailer-container {
-  position: relative;
+.wrapper-out {
+  z-index: 999;
+  position: fixed;
+  inset: 0;
   background-color: rgba(0, 0, 0, 0.9);
-  width: 100%;
-  height: 0;
 
-  iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+  .wrapper-inner {
+    padding-top: 25vh;
+    max-width: 660px;
+    max-height: 415px;
+    margin: 0 auto;
+
+    .trailer-container {
+      position: relative;
+      padding-bottom: 56.25%;
+      // padding-top: 30px;
+      height: 0;
+      overflow: hidden;
+
+      iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
 }
 </style>
